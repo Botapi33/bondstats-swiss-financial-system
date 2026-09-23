57fb34811e5b11257819acadc0553c78da67ecbe
Botapi33 <71960375+Botapi33@users.noreply.github.com>
2026-09-23 23:40:56 +0200

    Delete astro.config.mjs

--- astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'https://botapi33.github.io',
-  base: '/bondstats-swiss-financial-system',
-  output: 'static',
-});
